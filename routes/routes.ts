ba8aae1294144e79b931a65b249e5b7a29e85b2d
Matheus Santos Carvalho <matheuscarvalho@inf.ufg.br>
2017-11-26 15:53:37 -0200

    Update routes.ts

--- routes/routes.ts
+++ routes/routes.ts
@@ -4,7 +4,6 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 const ObjectId = mongoose.Types.ObjectId;
-var cors = require('cors');
 
 const Anuncio = require('../models/anuncio');
 const Consumidor = require('../models/consumidor');
@@ -14,28 +13,24 @@ const Agendamento = require('../models/agendamento');
 const Compra = require('../models/compra');
 
 // Add headers
-router.use(cors());
-    /*function(req, res, next) {
+router.use(function(req, res, next) {
     
-    res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
     // Website you wish to allow to connect
-   // res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
-    //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-     //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
-    //res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Credentials', true);
 
     // Pass to next layer of middleware
     next();
-});*/
+});
 
 //Fluxo de caixa
 router.get('/fluxo/de/caixa/:id', (req, res, next) => {
